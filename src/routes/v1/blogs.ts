9b8b65aa1725f14a4a8a36357b182ce7d7abcc4e
SanjayShetty <shettysanjay719@gmail.com>
2025-07-12 18:02:44 +0530

    feat: added get blogs,get blog by userId,get blog by slug

--- src/routes/v1/blogs.ts
+++ src/routes/v1/blogs.ts
@@ -10,9 +10,16 @@ import uploadBlogBanner from '@/middlewares/uploadBlogBanner';
 
 //Controllers
 import createBlog from '@/controllers/v1/blog/create_blog';
-
+import getAllBlogs from '@/controllers/v1/blog/get_all_blogs';
+import getBlogsByUser from '@/controllers/v1/blog/get_blogs_by_user';
 //Validators
-import { createBlogValidation } from '@/utils/validators/blogValidators';
+import {
+  createBlogValidation,
+  getAllBlogsValidation,
+  getBlogsBySlugValidation,
+  getBlogsByUserValidation,
+} from '@/utils/validators/blogValidators';
+import getBlogBySlug from '@/controllers/v1/blog/get_blog_by_slug';
 
 const upload = multer();
 const router = Router();
@@ -26,6 +33,33 @@ router.post(
   createBlogValidation,
   validationError,
   createBlog,
+);
+
+router.get(
+  '/',
+  authenticate,
+  authorize(['admin', 'user']),
+  getAllBlogsValidation,
+  validationError,
+  getAllBlogs,
+);
+
+router.get(
+  '/user/:userId',
+  authenticate,
+  authorize(['admin', 'user']),
+  getBlogsByUserValidation,
+  validationError,
+  getBlogsByUser,
+);
+
+router.get(
+  '/:slug',
+  authenticate,
+  authorize(['admin', 'user']),
+  getBlogsBySlugValidation,
+  validationError,
+  getBlogBySlug,
 );
 
 export default router;
--- src/utils/validators/blogValidators.ts
+++ src/utils/validators/blogValidators.ts
@@ -1,4 +1,4 @@
-import { body } from 'express-validator';
+import { body,query,param } from 'express-validator';
 
 export const createBlogValidation = [
   body('title')
@@ -10,3 +10,34 @@ export const createBlogValidation = [
   body('content').trim().notEmpty().withMessage('Content is required'),
   body('status').optional().isIn(['draft','published']).withMessage('Status must be one of the value,draft or published',),
 ];
+
+export const getBlogsByUserValidation = [
+    param('userId')
+    .isMongoId()
+    .withMessage('Invalid user ID'),
+    query('limit')
+      .optional()
+      .isInt({ min: 1, max: 50 })
+      .withMessage('Limit must be between 1 to 50'),
+    query('offset')
+      .optional()
+      .isInt({ min: 0 })
+      .withMessage('Offset must be a positive integer'),
+]
+
+export const getBlogsBySlugValidation = [
+    param('slug')
+    .notEmpty()
+    .withMessage('Slug is required'),
+]
+
+export const getAllBlogsValidation = [
+    query('limit')
+      .optional()
+      .isInt({ min: 1, max: 50 })
+      .withMessage('Limit must be between 1 to 50'),
+    query('offset')
+      .optional()
+      .isInt({ min: 0 })
+      .withMessage('Offset must be a positive integer'),
+]
