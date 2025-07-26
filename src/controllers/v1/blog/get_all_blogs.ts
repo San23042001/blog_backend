e7606a70076f1a11a9099298c98e8b2fe12c9b3d
SanjayShetty <shettysanjay719@gmail.com>
2025-07-26 15:14:32 +0530

    feat: added get all post by name

--- src/controllers/v1/blog/get_all_blogs.ts
+++ src/controllers/v1/blog/get_all_blogs.ts
@@ -3,36 +3,47 @@ import { logger } from '@/lib/winston';
 import config from '@/config';
 
 //Models
-import Blog from '@/models/blog'
+import Blog from '@/models/blog';
 import User from '@/models/user';
 
 //Types
 import type { Request, Response } from 'express';
+
 interface QueryType {
-    status?:'draft'|'published'
+  status?: 'draft' | 'published';
+  $or?: Record<string, any>[];
 }
 
 const getAllBlogs = async (req: Request, res: Response): Promise<void> => {
   try {
     const userId = req.userId;
-    const limit = parseInt(req.query.limit as string) ?? config.defaultResLimit;
-    const offset =
-      parseInt(req.query.offset as string) ?? config.defaultResOffset;
- 
-    const user = await User.findById(userId).select('role').lean().exec();
-    const query:QueryType = {};
+    const limit = parseInt(req.query.limit as string) || config.defaultResLimit;
+    const offset = parseInt(req.query.offset as string) || config.defaultResOffset;
+    const search = (req.query.search as string)?.trim();
 
-    //Show only the published post to a normal user
-    if(user?.role === 'user'){
+    const user = await User.findById(userId).select('role').lean().exec();
+    const query: QueryType = {};
+
+    // Show only published posts to normal users
+    if (user?.role === 'user') {
       query.status = 'published';
     }
+
+    // Add search filter if provided
+    if (search) {
+      query.$or = [
+        { title: { $regex: search, $options: 'i' } }, // case-insensitive
+        { content: { $regex: search, $options: 'i' } },
+      ];
+    }
+
     const total = await Blog.countDocuments(query);
     const blogs = await Blog.find(query)
       .select('-banner.publicId -__v')
-      .populate('author','-createdAt -updatedAt -__v')
+      .populate('author', '-createdAt -updatedAt -__v')
       .limit(limit)
       .skip(offset)
-      .sort({createdAt:-1})
+      .sort({ createdAt: -1 })
       .lean()
       .exec();
 
